5bee7ceb963c5c1d569d5f1baf9c8f0374a288c2
bernadette1008 <vachiv@naver.com>
2025-09-09 09:29:01 +0900

    위치 검색 키워드 보이도록 수정

--- src/main/java/com/example/demo/model/PlaceInfo.java
+++ src/main/java/com/example/demo/model/PlaceInfo.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class PlaceInfo {
     private String name;
     private String category;
+    private String keyword;
     private String address;
     private String imageUrl;
     private String placeUrl;
@@ -16,6 +17,9 @@ public class PlaceInfo {
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
+    public String getKeyword() {return keyword; }
+    public void setKeyword(String keyword) { this.keyword = keyword; }
 
     public String getCategory() { return category; }
     public void setCategory(String category) { this.category = category; }
